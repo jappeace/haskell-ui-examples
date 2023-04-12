5209df9c72a98c5deffc987285ba727bc8fdef08
Jappie Klooster <jappieklooster@hotmail.com>
2023-04-12 20:11:05 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+# I used chatgpt to generate this template and then just
+# modified to how I normally use these things.
+{
+  description = "My Haskell project";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-compat }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      hpkgs = pkgs.haskell.packages.ghc944.override {
+        overrides = hnew: hold: {
+          template-project = hnew.callCabal2nix "template-project" ./. { };
+        };
+      };
+    in
+    {
+      defaultPackage.x86_64-linux =  hpkgs.template-project;
+      inherit pkgs;
+      devShell.x86_64-linux = hpkgs.shellFor {
+        packages = ps : [ ps."template-project" ];
+        withHoogle = true;
+
+        buildInputs = [
+          hpkgs.haskell-language-server
+          pkgs.ghcid
+          pkgs.cabal-install
+        ];
+      };
+    };
+}
